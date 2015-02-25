9908b7f57d239680ee245834baff9f75884120c5
Matthieu GUIGON <matguig@student.42.fr>
2015-02-25 22:01:49 +0100

    [IMPROV] comment for special cases

--- test/tests.c
+++ test/tests.c
@@ -6,7 +6,7 @@
 /*   By: matguig <matguig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/24 17:29:52 by cdannapp          #+#    #+#             */
-/*   Updated: 2015/02/24 20:45:32 by matguig          ###   ########.fr       */
+/*   Updated: 2015/02/25 22:00:35 by matguig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int main(void)
 
 	test_memcpy(10);
 	test_memcpy(30);
-	//test_memcpy(60);
+	test_memcpy(60);
 
 	test_puts("« On ne dit pas de mal devant celui qui n'éсοutе раs. » - Saint Augustin");
 	test_puts(NULL);
@@ -38,23 +38,43 @@ int main(void)
 	test_strdup("« Les gens bien portants sont des malades qui s'ignοrеnt. » - Jules Romains");
 	test_strdup("");
 	//test_strdup(NULL);
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+	| char	*strdup(const char *s1);									|
+	| Si s1 == NULL :													|
+	| * Original : Segmentation fault									|
+	| * Perso    : Segmentation fault 									|
+	\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 	test_strcat();
 
 	test_memset(0);
 	test_memset(5);
 	test_memset(9);
-	//test_memset2(10);
+	test_memset(11);
+
 
 	test_bzero(9);
 
 	test_strlen("« Réfléchis avec lenteur, mais exécute rapidеmеnt tеs déсisiοns. » - Isocrate");
-	//test_strlen(NULL); // Comportement OK mais segfault
 	test_strlen("");
+	//test_strlen(NULL);
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+	| size_t     strlen(const char *s);									|
+	| Si s == NULL :													|
+	| * Original : Segmentation fault									|
+	| * Perso    : Segmentation fault									|
+	\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 	test_strcpy("« On crie pour taire ce qui crie. » - Henri Michaux");
-	// test_strcpy(NULL); // Comportement OK mais segfault
-	test_strcpy("blom");
+	test_strcpy("");
+	//test_strcpy(NULL);
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+	| char		*strcpy(char *restrict dst, const char *restrict src);	|
+	| Si s == NULL :													|
+	| * Original : Segmentation fault									|
+	| * Perso    : Segmentation fault									|
+	\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 
 	test_cat();
 	return 0;
