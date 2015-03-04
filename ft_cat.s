fab47107bc1068d86cfca7b8f1b7fdcf9a32c237
Matthieu GUIGON <matguig@student.42.fr>
2015-03-04 15:36:14 +0100

    [IMPROV] CAT

--- ft_cat.s
+++ ft_cat.s
@@ -5,8 +5,8 @@
 ;                                                    +:+ +:+         +:+       ;
 ;   By: matguig <matguig@student.42.fr>            +#+  +:+       +#+          ;
 ;                                                +#+#+#+#+#+   +#+             ;
-;   Created: 2015/02/20 14:39:23 by cdannapp          #+#    #+#               ;
-;   Updated: 2015/02/27 16:32:16 by matguig          ###   ########.fr         ;
+;   Created: 2015/02/20 14:39:23 by matguig           #+#    #+#               ;
+;   Updated: 2015/03/04 15:35:41 by matguig          ###   ########.fr         ;
 ;                                                                              ;
 ; **************************************************************************** ;
 
@@ -14,52 +14,43 @@
 %define READ		3
 %define STDOUT		1
 %define WRITE		4
-%define BUFFER_SIZE 1500
+%define BUFFER_SIZE 256
 
 section .bss
 	buf resb	BUFFER_SIZE
 
 section .text
 	global _ft_cat
-	extern _ft_strlen
 	extern _ft_memset
 
 _ft_cat:
-	enter 16, 0					;initialisation
+	enter 16, 0
 	pushf
 
-	cmp edi, 0
-	jl end
+	mov r15, rdi
 
 	beginloop:
-		mov r15, rdi			;on sauve rdi (le filedescriptor) pour s'en servir apres
-
-		mov rdi, buf
+		lea rdi, [rel buf]
 		mov rsi, 0
 		mov rdx, BUFFER_SIZE
-		call _ft_memset			; on met tous les octets de buf a 0
+		call _ft_memset
 
+		mov rax, FTCALL(READ)		; Read Syscall
 		mov rdi, r15
-
-		mov rax, FTCALL(READ)
-		mov rsi, buf			; Stock dans buf
+		lea rsi, [rel buf]
 		mov rdx, BUFFER_SIZE
-		syscall					; on lit dans le fd BUFFER_SIZE octets qu'on met dans buf
+		syscall
 
-		mov r12, rax
+		jc end						; Controll read syscall flag
 
-		mov rdi, buf
-		call _ft_strlen			; on compte la longueur du buf qu'on a lut
-		mov rdx, rax
+		cmp rax, 0
+		je end
 
+		mov rdx, rax				; Rax Syscall Read lenght
 		mov rax, FTCALL(WRITE)
 		mov rdi, STDOUT
-		mov rsi, buf
-		syscall					; on ecrit les caractere sur la  sortie standard
-
-		mov rdi, r15
-		cmp r12, 0				; si le retour de read est 0 on arrete
-		je end
+		lea rsi, [rel buf]
+		syscall
 		jmp beginloop
 
 	end:
