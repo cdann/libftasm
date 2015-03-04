c426986a5730be559b36f941881c3383a8343987
Celine DANNAPPE <cdannapp@e2r3p3.42.fr>
2015-03-04 15:48:33 +0100

    [FIX] conflit solve

--- ft_cat.s
+++ ft_cat.s
@@ -5,13 +5,8 @@
 ;                                                    +:+ +:+         +:+       ;
 ;   By: cdannapp <cdannapp@student.42.fr>          +#+  +:+       +#+          ;
 ;                                                +#+#+#+#+#+   +#+             ;
-<<<<<<< HEAD
 ;   Created: 2015/02/20 14:39:23 by cdannapp          #+#    #+#               ;
-;   Updated: 2015/03/04 15:34:38 by cdannapp         ###   ########.fr         ;
-=======
-;   Created: 2015/02/20 14:39:23 by matguig           #+#    #+#               ;
-;   Updated: 2015/03/04 15:35:41 by matguig          ###   ########.fr         ;
->>>>>>> fab47107bc1068d86cfca7b8f1b7fdcf9a32c237
+;   Updated: 2015/03/04 15:47:37 by cdannapp         ###   ########.fr         ;
 ;                                                                              ;
 ; **************************************************************************** ;
 
@@ -35,11 +30,6 @@ _ft_cat:
 	mov r15, rdi
 
 	beginloop:
-<<<<<<< HEAD
-		mov r15, rdi			;on sauve rdi (le filedescriptor) pour s'en servir apres
-
-=======
->>>>>>> fab47107bc1068d86cfca7b8f1b7fdcf9a32c237
 		lea rdi, [rel buf]
 		mov rsi, 0
 		mov rdx, BUFFER_SIZE
@@ -47,13 +37,7 @@ _ft_cat:
 
 		mov rax, FTCALL(READ)		; Read Syscall
 		mov rdi, r15
-<<<<<<< HEAD
-
-		mov rax, FTCALL(READ)
-		lea rsi, [rel buf]			; Stock dans buf
-=======
 		lea rsi, [rel buf]
->>>>>>> fab47107bc1068d86cfca7b8f1b7fdcf9a32c237
 		mov rdx, BUFFER_SIZE
 		syscall
 
@@ -66,15 +50,8 @@ _ft_cat:
 		mov rax, FTCALL(WRITE)
 		mov rdi, STDOUT
 		lea rsi, [rel buf]
-<<<<<<< HEAD
-		syscall					; on ecrit les caractere sur la  sortie standard
-
-		mov rdi, r15
-		cmp r12, 0				; si le retour de read est 0 on arrete
-		je end
-=======
 		syscall
->>>>>>> fab47107bc1068d86cfca7b8f1b7fdcf9a32c237
+
 		jmp beginloop
 
 	end:
