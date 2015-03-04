6a9d51e9be8ee2960b11df6a4b15cdcc43e63987
Matthieu GUIGON <matguig@student.42.fr>
2015-03-04 15:06:25 +0100

    [IMPROV] use rep instruction

--- ft_strlen.s
+++ ft_strlen.s
@@ -5,28 +5,29 @@
 ;                                                    +:+ +:+         +:+       ;
 ;   By: matguig <matguig@student.42.fr>            +#+  +:+       +#+          ;
 ;                                                +#+#+#+#+#+   +#+             ;
-;   Created: 2015/02/18 18:20:17 by cdannapp          #+#    #+#               ;
-;   Updated: 2015/02/25 21:49:41 by matguig          ###   ########.fr         ;
+;   Created: 2015/02/18 18:20:17 by matguig           #+#    #+#               ;
+;   Updated: 2015/03/04 15:05:31 by matguig          ###   ########.fr         ;
 ;                                                                              ;
 ; **************************************************************************** ;
 
-section .text           					;Code Segment
-	   global _ft_strlen
-
+section .text
+	global	_ft_strlen
 
 _ft_strlen:
 	enter 16, 0
 	pushf
 
-	mov rax, 0							;put 0 in return register
-	beginloop:
-		mov bl, [rdi]
-		cmp bl, 0
-		je end
-		inc rax
-		inc rdi
-		jmp beginloop					;go back to bginning
-	end:
-		popf
-		leave
-		ret								;return result;
+	mov r12, rdi
+	mov rax, 0
+	mov rcx, 0
+
+	not rcx
+	cld
+	repne scasb
+	sub rdi, r12
+	dec rdi
+
+	mov rax, rdi
+	popf
+	leave
+	ret
