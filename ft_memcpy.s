78748caf34873aec71398133631c39144aa2b77d
Celine DANNAPPE <cdannapp@e2r3p3.42.fr>
2015-03-04 15:44:06 +0100

    [FIX] ft_memcpy and ft_strdup with rep

--- ft_memcpy.s
+++ ft_memcpy.s
@@ -3,10 +3,10 @@
 ;                                                        :::      ::::::::     ;
 ;   ft_memcpy.s                                        :+:      :+:    :+:     ;
 ;                                                    +:+ +:+         +:+       ;
-;   By: matguig <matguig@student.42.fr>            +#+  +:+       +#+          ;
+;   By: cdannapp <cdannapp@student.42.fr>          +#+  +:+       +#+          ;
 ;                                                +#+#+#+#+#+   +#+             ;
 ;   Created: 2015/02/22 14:42:15 by cdannapp          #+#    #+#               ;
-;   Updated: 2015/02/25 21:46:35 by matguig          ###   ########.fr         ;
+;   Updated: 2015/03/04 15:38:35 by cdannapp         ###   ########.fr         ;
 ;                                                                              ;
 ; **************************************************************************** ;
 
@@ -18,18 +18,10 @@ section        .text
 		pushf
 
 		mov rax, rdi
-		mov rcx, 0
-		beginloop:
-			cmp rcx, rdx			;verifie le compteur rcx
-			je end
-			mov r15b,[rsi + rcx]	;copie lettre a lettre rsi dans r15
-			mov [rdi + rcx],r15b	; copie la lettre de r15 dans rdi
-			cmp r15b, 0				;verifie la fin de la chaine
-			je end
-			inc rcx
-			jmp beginloop
+		mov rcx, rdx
+		cld
+		rep movsb
 
-		end:
-			popf
-			leave
-			ret
+		popf
+		leave
+		ret
