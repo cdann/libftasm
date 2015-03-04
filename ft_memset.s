0aba0c163bd095063ea2668cdf69c3c163904c73
Matthieu GUIGON <matguig@student.42.fr>
2015-03-04 15:07:21 +0100

    [IMPROV] use rep instruction

--- ft_memset.s
+++ ft_memset.s
@@ -5,32 +5,26 @@
 ;                                                    +:+ +:+         +:+       ;
 ;   By: matguig <matguig@student.42.fr>            +#+  +:+       +#+          ;
 ;                                                +#+#+#+#+#+   +#+             ;
-;   Created: 2015/02/20 14:39:23 by cdannapp          #+#    #+#               ;
-;   Updated: 2015/02/25 21:35:44 by matguig          ###   ########.fr         ;
+;   Created: 2015/02/20 14:39:23 by matguig           #+#    #+#               ;
+;   Updated: 2015/03/04 15:06:59 by matguig          ###   ########.fr         ;
 ;                                                                              ;
 ; **************************************************************************** ;
 
 ; ordre des arguments de fonction: rdi = str, rsi = c, rdx = n  retour: rax
+; extern void		*ft_memset(void *str, int c, size_t n);
 
-section .text           					;Code Segment
-	   global _ft_memset
+section .text
+	global	_ft_memset
 
 _ft_memset:
 	enter 16, 0
 	pushf
 
-	mov rcx, 0
-	mov rax, rdi
-
-	beginloop:
-		cmp rcx, rdx
-		je end
-		mov [rdi], rsi
-		inc rcx
-		inc rdi
-		jmp beginloop
-
-	end:
-		popf
-		leave
-		ret
+	xor al, al
+	mov rax, rsi
+	mov rcx, rdx
+	cld
+	rep stosb
+	popf
+	leave
+	ret
